a827a91d8d76f590c8054b9f348b11cbac4caf61
yapiu <dorovskih2005@gmial.com>
2024-04-25 20:51:07 +0300

    upd:

--- Myfloat_overflow.cpp
+++ Myfloat_overflow.cpp
@@ -1,27 +1,75 @@
 #include "Myfloat_overflow.h"
 
-void Myfloat_overflow :: print() {
+void Myfloat_overflow ::print() {
   char sg = ' ';
   if (sign) {
     sg = '-';
   }
-  // char ptr_c = ' ';
-  // char ptr_d = ' ';
   if (condition_print == 0 || condition_print == -1) {
-    // ptr_c = '*';
     printf("%c*%d.%04d ", sg, c, d);
   }
   if (condition_print == 1) {
-    // ptr_d = '*';
     printf("%c%d.*%04d ", sg, c, d);
   }
   if (condition_print == 2 || condition_print == -2) {
-    // ptr_c = '*';
-    // ptr_d = '*';
     printf("%c*%d.*%04d ", sg, c, d);
   }
   if (condition_print == 3) {
     printf("%c%d.%04d ", sg, c, d);
   }
-  // printf("%c%c%d.%c%04d ", sg, ptr_c, c, ptr_d, d);
+}
+Myfloat_overflow operator+(Myfloat_overflow f1, Myfloat_overflow f2) {
+  int condition, sum_d, sum_c;
+  bool sign;
+
+  if (f1.d < f2.d) {
+    condition = 0;
+
+  } else {
+    condition = 1;
+  }
+
+  if (f1.c + f2.c < 0) {
+    sign = true;
+  } else {
+    sign = false;
+  }
+
+  sum_c = abs(f1.c + f2.c);
+  sum_d = abs(f1.d + f2.d);
+
+  return Myfloat_overflow(sum_c, sum_d, condition, sign);
+}
+Myfloat_overflow operator-(Myfloat_overflow f1, Myfloat_overflow f2) {
+  f2.sign = !f2.sign;
+  if ((f1.c < 0 && f2.c > 0) || (f1.c > 0 && f2.c < 0)) {
+    f1.d *= -1;
+  }
+  f2.c *= -1;
+  f2.d *= -1;
+  return operator+(f1, f2);
+}
+Myfloat_overflow operator*(Myfloat_overflow f1, Myfloat_overflow f2) {
+  bool sign;
+  int condition = -1;
+
+  if (f1.c < 0 && f2.c < 0) {
+    sign = false;
+  } else if (f1.c < 0 || f2.c < 0) {
+    sign = true;
+  } else
+    sign = false;
+
+  int f1d = f1.d / 1000;
+  int f2d = f2.d / 1000;
+  int f1c = abs(f1.c);
+  int f2c = abs(f2.c);
+
+  int first_part = (f1c * 10 + f1d) * f2d;
+  int second_part = (f1c * 10 + f1d) * f2c * 10;
+
+  double multiply = (double)(first_part + second_part) / 100;
+  Myfloat_overflow result(multiply);
+
+  return Myfloat_overflow(result.c, result.d, condition, sign);
 }
